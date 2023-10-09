56f757fcce26ed828e44610e13547feef6cf7875
majidi pashaei <Majid Pashayi@DESKTOP-3ESOJKH>
2023-10-09 14:13:14 +0330

    complete strict api

--- API/Program.cs
+++ API/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 //Connections
 builder.Services.AddDbContext<StoreContext>(opt => opt.UseSqlite(conn));
 var app = builder.Build();
@@ -17,10 +18,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors(opt=>{
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+});
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
